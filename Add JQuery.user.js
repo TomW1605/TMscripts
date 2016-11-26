c708b020dce3e97e4ad54418e16e33a95b424321
Thomas White <TomW1605@users.noreply.github.com>
2016-11-26 13:50:59 +0800

    Update Add JQuery.user.js

--- Add JQuery.user.js
+++ Add JQuery.user.js
@@ -2,7 +2,7 @@
 // @name         Add JQuery
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Add JQuery to all sites you visit
+// @description  Add JQuery to all sites you visit if it isen't there already
 // @author       TomW1605
 // @match        http://*/*
 // @match        https://*/*
@@ -12,8 +12,15 @@
 (function() {
     'use strict';
 
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://code.jquery.com/jquery-3.1.1.min.js';
-    document.head.appendChild(script);
+    try {
+        console.log('jQuery '+jQuery.fn.jquery+' is already loaded');
+    } catch (e) {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.onload = function () {
+            console.log('jQuery '+jQuery.fn.jquery+' has been loaded');
+        };
+        script.src = 'https://code.jquery.com/jquery-3.1.1.min.js';
+        document.head.appendChild(script);
+    }
 })();
