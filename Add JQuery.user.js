3fc94fffe95c7316f19e7141e7c4bf1ebfb91392
Thomas White <TomW1605@users.noreply.github.com>
2016-11-26 13:02:03 +0800

    Update Add JQuery.user.js

--- Add JQuery.user.js
+++ Add JQuery.user.js
@@ -1,4 +1,19 @@
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://code.jquery.com/jquery-3.1.1.min.js';
-document.head.appendChild(script);
+// ==UserScript==
+// @name         Add JQuery
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Add JQuery to all sites you visit
+// @author       TomW1605
+// @match        http://*/*
+// @match        https://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://code.jquery.com/jquery-3.1.1.min.js';
+    document.head.appendChild(script);
+})();
