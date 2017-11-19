54aff014385653b44a8cd019936e8accd36e2de1
Thomas White <TomW1605@users.noreply.github.com>
2017-11-19 10:22:43 +0800

    Update Youtube: Space to Pause.user.js

--- Youtube: Space to Pause.user.js
+++ Youtube: Space to Pause.user.js
@@ -10,8 +10,8 @@
 // @include         http*://*.youtu.be/*
 // @include         http*://youtu.be/*
 // @supportURL   https://github.com/TomW1605/TMscripts/issues
-// @downloadURL  https://github.com/TomW1605/TMscripts/raw/master/Youtube%3A%20Space%20to%20Pause.user.js
-// @updateURL    https://github.com/TomW1605/TMscripts/raw/master/Youtube%3A%20Space%20to%20Pause.user.js
+// @downloadURL  https://github.com/TomW1605/TMscripts/raw/master/Youtube:%20Space%20to%20Pause.user.js
+// @updateURL    https://github.com/TomW1605/TMscripts/raw/master/Youtube:%20Space%20to%20Pause.user.js
 // ==/UserScript==
 
 var k = function(action){
