21154436ff55a8729e0e4de8f5a2cfb184299ba2
Thomas White <TomW1605@users.noreply.github.com>
2017-11-19 10:54:13 +0800

    Update Unlock A+.user.js

--- Unlock A+.user.js
+++ Unlock A+.user.js
@@ -6,8 +6,6 @@
 // @author       TomW1605
 // @match        http://www.litcharts.com/*
 // @grant        none
-// @downloadURL  https://github.com/TomW1605/TMscripts/raw/master/Unlock%20A+.user.js
-// @updateURL    https://github.com/TomW1605/TMscripts/raw/master/Unlock%20A+.user.js
 // ==/UserScript==
  
 (function() {
