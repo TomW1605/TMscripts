f936d7a4451d6b8691def9b3ff28066a41c78299
Thomas White <TomW1605@users.noreply.github.com>
2018-02-26 15:47:01 +0800

    Create sort iLecture classes.user.js

---
+++ sort iLecture classes.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         sort iLecture classes
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  sort iLecture classes
+// @author       You
+// @match        https://echo360.org.au/home
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $(".home-content-main").children().each(function(i,li){
+        $(".home-content-main").prepend(li);
+    });
+})();
