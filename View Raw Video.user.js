d1cd939d39dd52cee8f69a965c25a9fc5e14665a
Thomas White <TomW1605@users.noreply.github.com>
2018-04-09 22:46:47 +0800

    Create View Raw Video.user.js

---
+++ View Raw Video.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         View Raw Video
+// @version      0.1
+// @description  View Raw Video
+// @author       TomW1605
+// @match        https://www.youtube.com/watch?v=*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var video = ytplayer.config.args.url_encoded_fmt_stream_map.split(",")[0].split("&");
+    var info = {};
+    for (var i in video) {
+        var temp = video[i].split("=");
+        info[temp[0]] = decodeURIComponent(temp[1]);
+    }
+    var url = info.url;
+    window.open(url,'_blank');
+})();
