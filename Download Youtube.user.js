4278d90329720a4783f44fcd2d28e9c88e6fc2ae
Thomas White <TomW1605@users.noreply.github.com>
2018-04-09 22:47:10 +0800

    Create Download Youtube.user.js

---
+++ Download Youtube.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Download Youtube
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Download Youtube
+// @author       TomW1605
+// @match        https://www.youtube.com/watch?v=*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var video = ytplayer.config.args.url_encoded_fmt_stream_map.split(",")[0].split("&");
+    var info = {};
+    for (var i in video) {
+        var temp = video[i].split("=");
+        info[temp[0]] = decodeURIComponent(temp[1]);
+    }
+    var url = info.url+"#"+encodeURI(ytplayer.config.args.title);
+    window.open(url,'_blank');
+})();
