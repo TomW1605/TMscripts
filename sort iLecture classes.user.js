9da0da79df01d8335868317b29e766278da651ba
Thomas White <TomW1605@users.noreply.github.com>
2018-04-09 23:20:46 +0800

    Update sort iLecture classes.user.js

--- sort iLecture classes.user.js
+++ sort iLecture classes.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  sort iLecture classes
-// @author       You
+// @author       TomW1605
 // @match        https://echo360.org.au/home
 // @grant        none
 // ==/UserScript==
