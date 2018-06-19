91a6db3276e14933508859bef18f521fceac1580
Thomas White <TomW1605@users.noreply.github.com>
2018-06-19 18:19:38 +0800

    Create symbolab popup remover.user.js

---
+++ symbolab popup remover.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         symbolab popup remover
+// @namespace    TomW1605
+// @version      0.1
+// @description  removes the popup symbolab shows after it has calculated the resault
+// @author       TomW1605
+// @match        https://www.symbolab.com/*
+// @grant        none
+// @run-at       document-idle
+// @supportURL   https://github.com/TomW1605/TMscripts/issues
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function waitForEl(selector, callback){
+        var poller1 = setInterval(function(){
+            var jObject = jQuery(selector);
+            if(jObject.length < 1){
+                return;
+            }
+            clearInterval(poller1);
+            callback(jObject);
+        },100);
+    }
+
+    waitForEl("[id^=tooltipster-] > div.tooltipster-box > div > div > a", function() {
+        $("[id^=tooltipster-] > div.tooltipster-box > div > div > a")[0].click();
+    });
+})();
