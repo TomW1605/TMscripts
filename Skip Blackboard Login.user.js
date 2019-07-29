5632c214fe17f97ec6a8130f422542b82ba29c51
Thomas White <TomW1605@users.noreply.github.com>
2019-07-29 11:32:41 +0800

    Create Skip Blackboard Login.user.js

---
+++ Skip Blackboard Login.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Skip Blackboard Login
+// @namespace    TomW1605
+// @version      0.1
+// @description  Skip Blackboard Login
+// @author       TomW1605
+// @match        https://lms.curtin.edu.au/webapps/login/
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.1.1.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    jQuery("#loginRedirectProviderList > li > a")[0].click()
+})();
