7586e3d45048dd35ac0d3f19aea32cb6c2bc9763
Thomas White <TomW1605@users.noreply.github.com>
2019-07-29 11:31:08 +0800

    Create Skip Curtin SSO.user.js

---
+++ Skip Curtin SSO.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Skip Curtin SSO
+// @namespace    TomW1605
+// @version      0.2
+// @description  Skip Curtin SSO
+// @author       TomW1605
+// @match        https://sso.curtin.edu.au/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var numIntervals = 0;
+    var pwFilledTimer = setInterval(function() {
+        var usrNameInp = document.getElementById("idToken1");
+        if (usrNameInp && usrNameInp.value != "") {
+
+            var passWrdInp = document.getElementById("idToken2");
+            if (passWrdInp && passWrdInp.value != "") {
+
+                clearInterval(pwFilledTimer);
+
+                var submitButton = document.getElementById("loginButton_0");
+                var clickEvent = document.createEvent('MouseEvents');
+                clickEvent.initEvent('click', true, true);
+                submitButton.dispatchEvent(clickEvent);
+            }
+        }
+        numIntervals++;
+        if (numIntervals > 10) {
+            clearInterval(pwFilledTimer);
+        }
+    },200);
+})();
