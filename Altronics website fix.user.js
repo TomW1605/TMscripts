b76e04e5c8ccaee4aeb6622ea33701ddeb0c35f8
Thomas White <TomW1605@users.noreply.github.com>
2019-12-14 08:54:40 +0800

    update to account for website changes

--- Altronics website fix.user.js
+++ Altronics website fix.user.js
@@ -1,27 +1,27 @@
 // ==UserScript==
 // @name         Altronics website fix
 // @namespace    TomW1605
-// @version      4.0
+// @version      5.0
 // @description  fix the sorting and the number of items per page showne on the altronics site
 // @author       TomW1605
 // @run-at       document-start
-// @match        https://www.altronics.com.au/audio-visual/*/*
-// @match        https://www.altronics.com.au/pro-audio/*/*
-// @match        https://www.altronics.com.au/speakers/*/*
-// @match        https://www.altronics.com.au/security/*/*
-// @match        https://www.altronics.com.au/test-measurement/*/*
-// @match        https://www.altronics.com.au/tools/*/*
-// @match        https://www.altronics.com.au/tech-data/*/*
-// @match        https://www.altronics.com.au/projects/*/*
-// @match        https://www.altronics.com.au/greenenergy-lighting/*/*
-// @match        https://www.altronics.com.au/power-supplies/*/*
-// @match        https://www.altronics.com.au/transformers/*/*
-// @match        https://www.altronics.com.au/hardware/*/*
-// @match        https://www.altronics.com.au/electronic-components/*/*
-// @match        https://www.altronics.com.au/switches/*/*
-// @match        https://www.altronics.com.au/connectors/*/*
-// @match        https://www.altronics.com.au/cable/*/*
-// @match        https://www.altronics.com.au/books/*/*
+// @match        https://www.altronics.com.au/audio-visual/*
+// @match        https://www.altronics.com.au/pro-audio/*
+// @match        https://www.altronics.com.au/speakers/*
+// @match        https://www.altronics.com.au/security/*
+// @match        https://www.altronics.com.au/test-measurement/*
+// @match        https://www.altronics.com.au/tools/*
+// @match        https://www.altronics.com.au/tech-data/*
+// @match        https://www.altronics.com.au/projects/*
+// @match        https://www.altronics.com.au/greenenergy-lighting/*
+// @match        https://www.altronics.com.au/power-supplies/*
+// @match        https://www.altronics.com.au/transformers/*
+// @match        https://www.altronics.com.au/hardware/*
+// @match        https://www.altronics.com.au/electronic-components/*
+// @match        https://www.altronics.com.au/switches/*
+// @match        https://www.altronics.com.au/connectors/*
+// @match        https://www.altronics.com.au/cable/*
+// @match        https://www.altronics.com.au/books/*
 // @match        https://www.altronics.com.au/search/*
 // @match        https://www.altronics.com.au/specials/*
 // @match        https://www.altronics.com.au/new/*
