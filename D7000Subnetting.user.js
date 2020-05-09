2a2ef7c0969ada9326a079f1834595ff2772c746
Thomas White <TomW1605@users.noreply.github.com>
2020-05-09 16:11:35 +0800

    remove jQuery from D7000Subnetting

--- D7000Subnetting.user.js
+++ D7000Subnetting.user.js
@@ -1,18 +1,16 @@
 // ==UserScript==
 // @name         D7000Subnetting
 // @namespace    TomW1605
-// @version      0.1
+// @version      0.2
 // @description  Enamble subnetting on D7000 IP resuervation page
 // @author       TomW1605
 // @match        http://192.168.0.1/*
 // @grant        none
 // @supportURL   https://github.com/TomW1605/TMscripts/issues
-// @require      https://code.jquery.com/jquery-3.1.1.min.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    jQuery.noConflict();
-    jQuery('input[name="rsv_ip3"]').removeAttribute('disabled')
+    document.getElementsByName("rsv_ip3").disabled = false;
 })();
