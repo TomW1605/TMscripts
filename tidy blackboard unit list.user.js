2a8970dc1ad239d959024151bc4f8861beea4c67
Thomas White <TomW1605@users.noreply.github.com>
2022-02-25 19:00:38 +0800

    Update tidy blackboard unit list.user.js

--- tidy blackboard unit list.user.js
+++ tidy blackboard unit list.user.js
@@ -77,12 +77,12 @@
     });
 
     //2022
-    jQuery("a:contains(' (Semester 1 2022 Bentley Campus - INT[1])')").text(function(index, text) {
+    jQuery("a:contains(' (Semester 1 2022 Bentley Perth Campus - INT[1])')").text(function(index, text) {
         var newText = text.replace(" (Semester 1 2022 Bentley Campus - INT[1])", "");
         console.log(newText);
         return newText;
     });
-    jQuery("a:contains(' (Semester 2 2022 Bentley Campus - INT[1])')").text(function(index, text) {
+    jQuery("a:contains(' (Semester 2 2022 Bentley Perth Campus - INT[1])')").text(function(index, text) {
         var newText = text.replace(" (Semester 2 2022 Bentley Campus - INT[1])", "");
         console.log(newText);
         return newText;
